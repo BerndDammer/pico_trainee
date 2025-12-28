9867547d40e72047da7d175d801f995cffa6ba41
BerndDammer <BerndDammer@web.de>
2025-12-28 17:34:50 +0100

    feierabend

--- pdp11_step1/src/pdp11_step1.c
+++ pdp11_step1/src/pdp11_step1.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
+#include "pdp11_defs.h"
 
 int main()
 {
     stdio_init_all();
-
+    
+    cpu_set_boot(0);
+    
     while (true) {
         printf("Hello, world!\n");
         sleep_ms(1000);
