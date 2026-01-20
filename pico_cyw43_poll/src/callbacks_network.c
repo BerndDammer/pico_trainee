fa3cbd1f15187cd71e7c8e0a317478766ac7dbf8
BerndDammer <BerndDammer@web.de>
2026-01-20 23:02:32 +0100

    bulk connect

--- pico_cyw43_poll/src/callbacks_network.c
+++ pico_cyw43_poll/src/callbacks_network.c
@@ -1,30 +1,39 @@
 #include "cyw43.h"
 
-
 /////////////////////////////////////////////////////////////////////
 ////////////////////////////// callbacks for network stack
-void cyw43_cb_tcpip_init(cyw43_t *self, int itf) 
+void cyw43_cb_tcpip_init(cyw43_t *self, int itf)
 {
     puts("CB init\n");
 }
-void cyw43_cb_tcpip_deinit(cyw43_t *self, int itf) 
+void cyw43_cb_tcpip_deinit(cyw43_t *self, int itf)
 {
     puts("CB de init\n");
 }
-void cyw43_cb_tcpip_set_link_up(cyw43_t *self, int itf) 
+void cyw43_cb_tcpip_set_link_up(cyw43_t *self, int itf)
 {
     puts("CB de link up\n");
 }
-void cyw43_cb_tcpip_set_link_down(cyw43_t *self, int itf) 
+void cyw43_cb_tcpip_set_link_down(cyw43_t *self, int itf)
 {
     puts("CB de link down\n");
 }
-void cyw43_cb_process_ethernet(void *cb_data,int itf, size_t len, const uint8_t *buf) 
+
+
+volatile uint8_t p[256];
+void cyw43_cb_process_ethernet(void *cb_data, int itf, size_t len, const uint8_t *buf)
 {
-    puts("CB ethernet frame\n");   
-}
-int cyw43_tcpip_link_status(cyw43_t *self, int itf)
-{
-    puts("CB status\n");   
+    for (int i = 0; i < 256; i++)
+    {
+        p[i] = buf[i];
+    }
+    puts("CB ethernet frame\n");
+    printf("FrameType %04X", p[13]<<8|p[12]);
 }
 
+int cyw43_tcpip_link_status(cyw43_t *self, int itf)
+{
+    puts("CB status\n");
+    printf("State %i\n", self->itf_state);
+    printf("cyw43_wifi_link_status %i\n", cyw43_wifi_link_status(self, CYW43_ITF_STA));
+}
